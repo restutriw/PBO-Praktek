23697f1c50baa26fe2b24073d1c7d60982c77033
Restu Triwahyuny <80731252+restutriw@users.noreply.github.com>
2021-10-10 20:24:32 +0700

    source code kasus 2

---
+++ Minggu 4/Item.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minggu.pkg4.kasus.pkg2;
+
+/**
+ *
+ * @author restu
+ */
+public class Item {
+        private String name;
+        private Item() {
+            name = "Ipin";
+        }
+        public Item(String name) { 
+            this.name = "ipin";
+            System.out.println(this.name);
+        }
+    }
+
+
+
---
+++ Minggu 4/UpinIpin.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minggu.pkg4.kasus.pkg2;
+
+/**
+ *
+ * @author restu
+ */
+public class UpinIpin {
+
+    public static void main(String[] args) {
+        Item name = new Item("upin");
+    }
+    
+}
