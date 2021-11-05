babddf7be60e4f72445b9466108b4e10f330e0bf
Restu Triwahyuny <80731252+restutriw@users.noreply.github.com>
2021-11-06 00:25:20 +0700

    Source code kasus 1

---
+++ Minggu 1/Kasus 1/Minggu7Kasus1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minggu.pkg7.kasus.pkg1;
+
+/**
+ *
+ * @author restu
+ */
+public class Minggu7Kasus1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
